125ac937bed8738b04ba32dd3f5c4cadf55501c5
kshitijSharma <43933347+kshitijsharma0506@users.noreply.github.com>
2020-04-12 11:14:53 +0530

    JS files.
    JSON file contains the details about the product and JS file contain all the functionality of the buttons and other functions.

---
+++ mainFile.js
@@ -0,0 +1,43 @@
+//All the variables name
+const cartBtn=document.querySelector('.cart-btn');
+const allProducts=document.querySelector('.product-center');
+const addBtn=document.querySelector('.bag-btn');
+
+const closeCartBtn=document.querySelector('.close-cart');
+const cartClear=document.querySelector('.clear-cart');
+const incItem=document.querySelector('.fa-plus');
+const decItem=document.querySelector('.fa-minus');
+const removeItm=document.querySelector('.remove-item');
+
+
+
+
+
+const cartDOM=document.querySelector(".cart")
+const cartOverlayDOM=doc.que(".cart-overlay")
+const cartItems=doc.que(".cart-item")
+const cartTotal =doc.que(".cart-total")
+const cartDOM=doc.que(".cart-content")
+
+
+
+
+
+
+
+
+//All the variables name
+const allProducts=document.querySelector('.product-center');
+const addBtn=document.querySelector('.bag-btn');
+
+const removeItm=document.querySelector('.remove-item');
+
+
+
+const cartDOM=doc.que(".cart")
+const cartOverlayDOM=doc.que(".cart-overlay")
+const cartItems=doc.que(".cart-item")
+const cartTotal =doc.que(".cart-total")
+const cartDOM=doc.que(".cart-content")
+
+
