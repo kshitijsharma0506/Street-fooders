2343b874f1e48d161bc2ede9befeef037c29c8a5
kshitijSharma <43933347+kshitijsharma0506@users.noreply.github.com>
2020-04-16 13:25:02 +0530

    Updating the buttons functionality.

--- mainFile.js
+++ mainFile.js
@@ -1,43 +1,76 @@
 //All the variables name
 const cartBtn=document.querySelector('.cart-btn');
-const allProducts=document.querySelector('.product-center');
-const addBtn=document.querySelector('.bag-btn');
-
+const cartDOM=document.querySelector(".cart");
+const cartOverlayDOM=document.querySelector(".cart-overlay");
+const productsDOM=document.querySelector('.products-center');
 const closeCartBtn=document.querySelector('.close-cart');
 const cartClear=document.querySelector('.clear-cart');
-const incItem=document.querySelector('.fa-plus');
-const decItem=document.querySelector('.fa-minus');
-const removeItm=document.querySelector('.remove-item');
+const cartItems=document.querySelector(".cart-item");
+const cartTotal =document.querySelector(".cart-total")
+const cartContent=document.querySelector(".cart-content")
 
 
+//Placing all the obj taking and getting info form this cart only;
+let mainCart=[];
+
+class getProducts{
+    async Products(){
+        try{
+            let promise=await fetch('product.json');
+            let data=await promise.json();
+            let prod=data.items;
+            prod=prod.map(item=>{
+                const {title,price}=item.fields;
+                const {id}=item.sys;
+                const img=item.fields.image.fields.file.url;
+                return {title,price,id,img}
+            })
+            return prod;
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+}
+
+class display{
+    displayProducts(products){
+        let result='';
+        products.forEach(product=>{
+            result+= `
+                <article class="product">
+                    <div class="img-container">
+                        <img src=${product.img} alt="Image-one" class="product-img">
+                        <button type="button" class="bag-btn" data-id=${product.id}>
+                            <i class="fas fa-shopping-cart">
+                            Add to cart
+                            </i>
+                        </button>
+                    </div>
+                    <h3>${product.title}</h3>
+                    <h4><i class="fas fa-rupee-sign"></i>${product.price}</h4>
+                </article>
+            `;
+        });
+        productsDOM.innerHTML=result;
+    }
+}
+
+class storage{
+    constructor(item){
+
+    }
+}
+
+/*
+The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading. 
+*/
 
 
+document.addEventListener("DOMContentLoaded",()=>{
+    let ui=new display();
+    let porducts=new getProducts(); 
 
-const cartDOM=document.querySelector(".cart")
-const cartOverlayDOM=doc.que(".cart-overlay")
-const cartItems=doc.que(".cart-item")
-const cartTotal =doc.que(".cart-total")
-const cartDOM=doc.que(".cart-content")
-
-
-
-
-
-
-
-
-//All the variables name
-const allProducts=document.querySelector('.product-center');
-const addBtn=document.querySelector('.bag-btn');
-
-const removeItm=document.querySelector('.remove-item');
-
-
-
-const cartDOM=doc.que(".cart")
-const cartOverlayDOM=doc.que(".cart-overlay")
-const cartItems=doc.que(".cart-item")
-const cartTotal =doc.que(".cart-total")
-const cartDOM=doc.que(".cart-content")
-
-
+    //getting the products 
+    porducts.Products().then(products=>ui.displayProducts(products));
+});
